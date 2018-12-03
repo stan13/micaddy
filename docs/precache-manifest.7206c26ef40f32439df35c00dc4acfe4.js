4023f7ec7fd97bb6d6b586d1f8f473b35dca4e13
stan13 <nathans@miclub.com.au>
2018-12-04 03:14:58 +0800

    initial demo

---
+++ docs/precache-manifest.7206c26ef40f32439df35c00dc4acfe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffd69f6e0c6b530fcdd1",
+    "url": "/static/css/main.37b7b558.chunk.css"
+  },
+  {
+    "revision": "ffd69f6e0c6b530fcdd1",
+    "url": "/static/js/main.ffd69f6e.chunk.js"
+  },
+  {
+    "revision": "b8ea769401f5acb1d737",
+    "url": "/static/js/1.b8ea7694.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f212be4de3b355a11b040026ee8fb8c2",
+    "url": "/index.html"
+  }
+];
